3222bd3a5db4a68113c9a2b1b285970547b64833
EliasCueto45 <ecuetofficial@gmail.com>
2025-12-29 17:32:25 -0500

    Fixed comma

--- open-mat/server/openmat_proj/urls.py
+++ open-mat/server/openmat_proj/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/gyms', include("gym_app.urls"))
+    path('api/gyms', include("gym_app.urls")),
     path('api/auth/', include('user_app.urls')),
     path("api/", include("review_app.urls")),
 
