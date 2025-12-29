85952c4eddaedf15a6468d093a120dd7a9f3f94d
arnold1945 <vscj58@gmail.com>
2025-12-29 16:19:46 -0600

    fixed user_app urls line 8

--- open-mat/server/user_app/urls.py
+++ open-mat/server/user_app/urls.py
@@ -5,7 +5,7 @@ from .views import Register, Login, Logout, Info, UserView
 
 urlpatterns = [
     
-    path('user/:id', Info.as_view(),name='info'),
+    path('user/<int:id>', Info.as_view(),name='info'),
     path('register/', Register.as_view(), name='register'),
     path('login/', Login.as_view(), name='login'),
     path('logout/', Logout.as_view(), name='logout'),
