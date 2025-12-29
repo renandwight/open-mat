72663e7fe054685cbe00f6460da2469c216a29ff
Chris Sorley <inc3ndi4ry@gmail.com>
2025-12-29 10:19:07 -0600

    Feature Event: Event Model and Serializer drafted

---
+++ open-mat/server/event_app/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+class Event(models.Model):
+    gym=models.ForeignKey(Gym, on_delete=models.CASCADE, related_name='gym_events')
+    user=models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_events')
+    event_date=models.DateTimeField()
+    gi=models.BooleanField()
+    fee=models.DecimalField(max_digits=5, decimal_places=2)
+    open_class=models.BooleanField()
+    created_at= models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.gym} - {self.user} - {self.event_date}"
