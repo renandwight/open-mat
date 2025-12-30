62d469fa4a136e91cff52148916ef265fc6c11a0
EliasCueto45 <ecuetofficial@gmail.com>
2025-12-30 16:43:58 -0500

    Minor fixes to gym

--- open-mat/server/gym_app/urls.py
+++ open-mat/server/gym_app/urls.py
@@ -3,7 +3,7 @@ from .views import All_Gyms, A_Gym, Nearby_Gyms,Filtered_Gyms
 
 urlpatterns = [
     path('', All_Gyms.as_view(), name='gyms-list'),
-    path('<int:gym_id>/', A_Gym.as_view(), name='detailed-gym'),
+    path('<int:id>/', A_Gym.as_view(), name='detailed-gym'),
     path('search/',Filtered_Gyms.as_view(), name="search-results"),
     path('nearby/', Nearby_Gyms.as_view(), name='nearby-gyms' )
 ]
