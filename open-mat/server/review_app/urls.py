89995322d069e6ecd43b6c323c44ed66fe8e1041
Chris Sorley <inc3ndi4ry@gmail.com>
2025-12-30 15:18:43 -0600

    updated main urls.py and reviews urls.py

--- open-mat/server/review_app/urls.py
+++ open-mat/server/review_app/urls.py
@@ -3,6 +3,6 @@ from .views import GymReviews, CreateReview, ReviewDetail
 
 urlpatterns = [
     path("gyms/<int:gym_id>/reviews/", GymReviews.as_view(), name="gym-reviews"),
-    path("reviews/", CreateReview.as_view(), name="create-review"),
-    path("reviews/<int:id>/", ReviewDetail.as_view(), name="review-detail"),
+    path("", CreateReview.as_view(), name="create-review"),
+    path("<int:id>/", ReviewDetail.as_view(), name="review-detail"),
 ]
