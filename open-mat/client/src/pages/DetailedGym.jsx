54db1a8aae1b5292ce86459e171c98e96ae5dc3e
EliasCueto45 <ecuetofficial@gmail.com>
2026-01-05 15:05:03 -0500

    Added nearby and search gyms to frontend, minor fix to review, mademigrations

--- open-mat/client/src/pages/DetailedGym.jsx
+++ open-mat/client/src/pages/DetailedGym.jsx
@@ -1,6 +1,8 @@
 import {api} from '../api/api'
 import {useParams} from 'react-router-dom'
 import { useEffect, useState} from 'react';
+import Eventcard from '../components/Eventcard';
+import {Link} from 'react-router-dom'
 
 
 export default function DetailedGym(){
@@ -22,13 +24,17 @@ export default function DetailedGym(){
 
     if (!gymData) return <div>Loading...</div>;
 
-    const {name, street, city, state, zip, gym_events = []} = gymData;
+    const {name, street, city, state, zip, gym_events, reviews = []} = gymData;
 
     return (
         <div className='border-2'>
             <h2>{name}</h2>
             <h3>{street} {city}, {state} {zip}</h3>
-            <h3>Events: {gym_events.map(e => e.event_date).join(", ")}</h3>
+             {gym_events.map((k,v)=>(
+                <Eventcard key={k.id} eventsData={k} />
+            ))}
+            <h3>Reviews:</h3>
+            {reviews.length>0 ? <h3> {reviews.map(r=>r.rating)}</h3> : <h3>This gym has no reviews? <Link>Add one?</Link></h3>}
             <small>Gym ID: {id}</small>
         </div>
     )
