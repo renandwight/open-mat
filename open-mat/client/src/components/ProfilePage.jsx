ac23f51b8b0fbf65a4dd3cf19b80373f7f1d55c2
arnold1945 <vscj58@gmail.com>
2026-01-05 12:04:59 -0600

    arnold - setting up ProfilePage.jsx

---
+++ open-mat/client/src/components/ProfilePage.jsx
@@ -0,0 +1,7 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
+import axios from "axios";
+
+
+
---
+++ open-mat/client/src/context/AuthContext.jsx
@@ -0,0 +1,53 @@
+import { createContext, useContext, useState, useEffect } from "react";
+import axios from "axios";
+
+
+const AuthContext = createContext();
+
+export function AuthProvider({ children }){
+    const [token, setToken ] = useState(localStorage.getItem('token'));
+    const [user, setUser] = useState(null);
+
+    const login = (token) => {
+        localStorage.setItem('token', token);
+        setToken(token);
+    };
+
+    const logout = () => {
+        localStorage.removeItem('token');
+        setToken(null);
+        setUser(null);
+
+    };
+    useEffect(() =>{
+        if(!token) return;
+
+        const fetchUser = async () => {
+            try {
+                const res = await axios.get("http://127.0.0.1:8000/api/auth/user/", {
+                    headers: {
+                        Authorization: `Token ${token}`,
+
+                    },
+                });
+                setUser(res.data);
+                
+            } catch(err) {
+                console.error("failed to fetch", err);
+                logout();
+            };
+        };
+        fetchUser();
+    }, [token]);
+
+    const isAuthenticated = !!token;
+
+    return (
+        <AuthContext.Provider value={{token, user, login, logout, isAuthenticated}}>
+            {children}
+        </AuthContext.Provider>
+    );
+
+}
+
+export const useAuth = () => useContext(AuthContext);
