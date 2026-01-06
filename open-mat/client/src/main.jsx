dd7a58f710ca98b38426943e2996bf2169e7d02e
arnold1945 <vscj58@gmail.com>
2026-01-06 02:12:45 -0600

    imported AuthProvider in main.jsx

--- open-mat/client/src/main.jsx
+++ open-mat/client/src/main.jsx
@@ -2,9 +2,11 @@ import './index.css';
 import router from './router';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
-
+import { AuthProvider } from './context/AuthContext';
 import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+    <AuthProvider>
     <RouterProvider router={router}/>
+    </AuthProvider>
 )
