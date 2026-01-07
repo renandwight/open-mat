dd2e7789bb93a674b3dd2f2043dbd6ab05bf7600
autreybrycen <brycen.autrey@icloud.com>
2026-01-07 14:07:21 -0800

    server only returns username

--- open-mat/client/src/components/ReviewCard.jsx
+++ open-mat/client/src/components/ReviewCard.jsx
@@ -1,17 +1,19 @@
 import { Card, Container, ListGroup, Button } from 'react-bootstrap';
 
 export default function ReviewCard({ reviewData }) {
-  const { rating, comment, created_at, user } = reviewData;
+  const { rating, comment, created_at, client } = reviewData;
+  const rawName = client?.username || client?.email || "";
+  const displayName = rawName.includes("@") ? rawName.split("@")[0] : rawName;
 
   return (
     <Container className='d-flex justify-content-center'>
       <Card style={{width: '18rem'}}>
         <ListGroup>
+          {displayName && <ListGroup.Item>Reviewer: {displayName}</ListGroup.Item>}
           <ListGroup.Item>Rating: {rating}</ListGroup.Item>
           <ListGroup.Item>Comment: {comment}</ListGroup.Item>
           <ListGroup.Item>Created: {created_at}</ListGroup.Item>
         </ListGroup>
-        {/* {user && <li>Reviewer: {user.username}</li>} */}
       </Card>
     </Container>
   );
--- open-mat/server/review_app/serializers.py
+++ open-mat/server/review_app/serializers.py
@@ -1,10 +1,25 @@
-from rest_framework.serializers import ModelSerializer
-from user_app.serializers import ClientSerializer
+from rest_framework.serializers import ModelSerializer, SerializerMethodField
+from user_app.models import Client
 from .models import Review
+
+class ReviewClientSerializer(ModelSerializer):
+    username = SerializerMethodField()
+
+    class Meta:
+        model = Client
+        fields = ["username"]
+
+    def get_username(self, client):
+        if client.username:
+            if "@" in client.username:
+                return client.username.split("@")[0]
+            return client.username
+        email = client.email or ""
+        return email.split("@")[0] if "@" in email else email
 
 # serializer to GET
 class ReviewReadSerializer(ModelSerializer):
-    client = ClientSerializer(read_only=True)
+    client = ReviewClientSerializer(read_only=True)
 
     class Meta:
         model = Review
