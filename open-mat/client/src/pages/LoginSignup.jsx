2cf4396934fb52dda19a58cb9c737799e328a0a8
arnold1945 <vscj58@gmail.com>
2026-01-09 11:02:45 -0600

    changed all the buttons in ProfilePage.jsx and LoginSignup.jsx to be uniform with the rest

--- open-mat/client/src/pages/LoginSignup.jsx
+++ open-mat/client/src/pages/LoginSignup.jsx
@@ -2,43 +2,44 @@ import { useState } from "react";
 import axios from "axios";
 import { useAuth } from "../context/AuthContext";
 import { useNavigate } from "react-router-dom";
+import { Button } from 'react-bootstrap';
 
 export default function LoginSignup() {
-    const [isSignup, setIsSignup] = useState(false);
-    const [formData, setFormData] = useState({
-        email:"",
-        password: "",
-    });
-    // adding for password error feature
-    const [error, setError] = useState("");
+  const [isSignup, setIsSignup] = useState(false);
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+  // adding for password error feature
+  const [error, setError] = useState("");
 
-    const {login} = useAuth();
-    const navigate = useNavigate();
+  const { login } = useAuth();
+  const navigate = useNavigate();
 
-    const handleChange = (e) =>{
-        setFormData({ ...formData, [e.target.name]: e.target.value});
-        setError(""); //adding this for error on typing
-      };
-    const handleSubmit = async (e) => {
-        e.preventDefault();
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+    setError(""); //adding this for error on typing
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-        const url = isSignup
-        ? "http://127.0.0.1:8000/api/auth/register/"
-        : "http://127.0.0.1:8000/api/auth/login/"
+    const url = isSignup
+      ? "http://127.0.0.1:8000/api/auth/register/"
+      : "http://127.0.0.1:8000/api/auth/login/"
 
-        try {
-        const res = await axios.post(url, formData);
-        login(res.data.token);
-        navigate("/");
-        } catch (err) {
-          if (!isSignup) {
-            setError("hey! wrong password!"); //warning when putting the wrong password when logging in
-          }
-        }
-    };
+    try {
+      const res = await axios.post(url, formData);
+      login(res.data.token);
+      navigate("/");
+    } catch (err) {
+      if (!isSignup) {
+        setError("hey! wrong password!"); //warning when putting the wrong password when logging in
+      }
+    }
+  };
 
-    return (
-        <div className="max-w-sm mx-auto mt-20 p-6 border rounded">
+  return (
+    <div className="max-w-sm mx-auto mt-20 p-6 border rounded">
       <h1 className="text-xl mb-4">
         {isSignup ? "Create Account" : "Login"}
       </h1>
@@ -48,31 +49,37 @@ export default function LoginSignup() {
           name="email"
           placeholder="Email"
           onChange={handleChange}
-          className="border p-2"
+          className="border p-2 me-3"
         />
         <input
           name="password"
           type="password"
           placeholder="Password"
           onChange={handleChange}
-          className="border p-2"
+          className="border p-2 me-3"
         />
         {/* to populate error message */}
         {error && <p className="text-red-600 text-sm">{error}</p>}
 
-        <button className="bg-black text-white p-2">
+        <Button
+          type="submit"
+          variant="outline-primary"
+        >
           {isSignup ? "Sign Up" : "Login"}
-        </button>
+        </Button>
+
       </form>
 
-      <button
+      <Button
+        variant="outline-primary"
+        className="mt-3"
         onClick={() => setIsSignup(!isSignup)}
-        className="text-sm mt-4 underline"
       >
+
         {isSignup
           ? "Already have an account? Login"
           : "Don't have an account? Sign up"}
-      </button>
+      </Button>
     </div>
-    );
+  );
 };
